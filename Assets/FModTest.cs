6b398f60cf90d016777b56621745b3631d05ecfc
Jun Zhong <jzhong@saicusa.com>
2017-11-09 13:50:26 -0800

    Add FMOD test code

---
+++ Assets/FModTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+
+public class FModTest : MonoBehaviour {
+
+	private FMOD.System system;
+
+	// Use this for initializations
+	void Start () {
+		FMOD.RESULT res = FMOD.Factory.System_Create (out system);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
